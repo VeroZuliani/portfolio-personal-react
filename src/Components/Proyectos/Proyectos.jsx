0ebe68ccaed1f562db3ababdd16b8fe08666a86e
Verónica Inés Zuliani <133280626+VeroZuliani@users.noreply.github.com>
2024-07-30 02:48:43 -0300

    Modifique buttonProyectos y animacion en Proyectos

--- src/Components/Proyectos/Proyectos.jsx
+++ src/Components/Proyectos/Proyectos.jsx
@@ -1,4 +1,4 @@
-import { Flex, Heading , Tabs, TabList, Tab, TabPanels, TabPanel, Link, Image, Center, Wrap} from "@chakra-ui/react"
+import { Flex, Heading , Tabs, TabList, Tab, TabPanels, TabPanel, Link, Image, Center, Wrap, WrapItem, Button} from "@chakra-ui/react"
 import { motion } from "framer-motion"
 
 
@@ -12,7 +12,6 @@ const Proyectos = () => {
             alignItems='center'
             flexDirection='column'
             bg='bg.white'
-            fontFamily='font.playfair'
             >
 
             <Heading as='h1' 
@@ -27,136 +26,116 @@ const Proyectos = () => {
             <Tabs variant='unstyled'>
 
                 <TabList className="btn-grupo" p='5' display='flex' justifyContent='center' alignItems='center' cursor='pointer' fontFamily='font.playfair'>
-                    <Tab className="btn btn-boton1" border='1px' borderStyle='solid' borderColor='text.formLabel' borderRadius='5px 0 0 5px' _selected={{bg:'text.color'}} transition={{bg:'1s',}} _hover={{bg:'text.color',}}>Todo el trabajo</Tab>
-                    <Tab className="btn btn-boton2" border='1px' borderStyle='solid' borderColor='text.formLabel' _selected={{bg:'text.color'}} transition={{bg:'1s',}} _hover={{bg:'text.color',}}>Diseño Web</Tab>
-                    <Tab className="btn btn-boton3" border='1px' borderStyle='solid' borderColor='text.formLabel' _selected={{bg:'text.color'}} transition={{bg:'1s',}} _hover={{bg:'text.color',}}>Diseño Mobile</Tab>
-                    <Tab className="btn btn-boton4" border='1px' borderStyle='solid' borderColor='text.formLabel' borderRadius='0 5px 5px 0' _selected={{bg:'text.color'}} transition={{bg:'1s',}} _hover={{bg:'text.color',}}>Diseño de Producto</Tab>
+                    <Tab className="btn btn-boton1" border='1px' borderStyle='solid' borderColor='text.formLabel' borderRadius='5px 0 0 5px' _selected={{bg:'text.color'}}>Todo el trabajo</Tab>
+                    <Tab className="btn btn-boton2" border='1px' borderStyle='solid' borderColor='text.formLabel' _selected={{bg:'text.color'}}>Diseño Web</Tab>
+                    <Tab className="btn btn-boton3" border='1px' borderStyle='solid' borderColor='text.formLabel' _selected={{bg:'text.color'}}>Diseño Mobile</Tab>
+                    <Tab className="btn btn-boton4" border='1px' borderStyle='solid' borderColor='text.formLabel' borderRadius='0 5px 5px 0' _selected={{bg:'text.color'}}>Diseño de Producto</Tab>
                 </TabList>
 
                 <TabPanels className="proyectos-contenedor">
                         
                     <TabPanel>
-                        <Wrap justify='space-between'  spacing='40px'
-                            w='700px' h='400px' mt='20px' 
-                            >
-                            <motion.WrapItem  
-                                whileHover={{ scale: [null, 1.1, 1.1] }}
-                                transition={{ duration: 0.3 }}
-                                >
-                                <Center>
-                                    <Link href="https://verozuliani.github.io/image-based-web-project/" isExternal><Image w='150px' h='150px' src="/proyecto1.png" alt=""/></Link>
-                                </Center>
-                            </motion.WrapItem>
-                            <motion.WrapItem 
-                                whileHover={{ scale: [null, 1.1, 1.1] }}
-                                transition={{ duration: 0.3 }}
-                                >
-                                <Center w='150px' h='150px' bg='green.200'>
-                                    <Link href="https://verozuliani.github.io/components-freelance-platform/" isExternal><Image w='150px' h='150px' src="/proyecto2.png" alt=""/></Link>
-                                </Center>
-                            </motion.WrapItem>
-                            <motion.WrapItem 
-                                whileHover={{ scale: [null, 1.1, 1.1] }}
-                                transition={{ duration: 0.3 }}
-                                >
-                                <Center w='150px' h='150px' bg='tomato'>
-                                    <Link href="https://verozuliani.github.io/xero-bank-loans/" isExternal><Image w='150px' h='150px' src="/proyecto3.png" alt=""/></Link>
-                                </Center>
-                            </motion.WrapItem>
-                            <motion.WrapItem 
-                                whileHover={{ scale: [null, 1.1, 1.1] }}
-                                transition={{ duration: 0.3 }}
-                                >
-                                <Center w='150px' h='150px' bg='blue.200'>
-                                    <Link href="https://verozuliani.github.io/photo-gallery/" isExternal><Image w='150px' h='150px' src="/proyecto4.png" alt=""/></Link>
-                                </Center>
-                            </motion.WrapItem>
-                            <motion.WrapItem 
-                                whileHover={{ scale: [null, 1.1, 1.1] }}
-                                transition={{ duration: 0.3 }}
-                                >
-                                <Center w='150px' h='150px' bg='blue.200'>
-                                    <Link href="https://verozuliani.github.io/API-Star-Wars/" isExternal><Image w='150px' h='150px' src="/proyecto5.png" alt=""/></Link>
-                                </Center>
-                            </motion.WrapItem>
-                            <motion.WrapItem 
-                                whileHover={{ scale: [null, 1.1, 1.1] }}
-                                transition={{ duration: 0.3 }}
-                                >
-                                <Center w='150px' h='150px' bg='blue.200'>
-                                    Box 6
-                                </Center>
-                            </motion.WrapItem>
+                        <Wrap justify='space-between'  spacing='40px' w='700px' h='400px' mt='20px'>
+                            <motion.div whileHover={{ scale: [null, 1.2, 1.2] }} transition={{ duration: 0.3 }}>
+                                <WrapItem>
+                                    <Center>
+                                        <Link href="https://verozuliani.github.io/image-based-web-project/" isExternal><Image w='150px' h='150px' src="/proyecto1.png" alt=""/></Link>
+                                    </Center>
+                                </WrapItem>
+                            </motion.div>
+                            <motion.div whileHover={{ scale: [null, 1.2, 1.2] }} transition={{ duration: 0.3 }}>
+                                <WrapItem>
+                                    <Center w='150px' h='150px' bg='green.200'>
+                                        <Link href="https://verozuliani.github.io/components-freelance-platform/" isExternal><Image w='150px' h='150px' src="/proyecto2.png" alt=""/></Link>
+                                    </Center>
+                                </WrapItem>
+                            </motion.div>
+                            <motion.div whileHover={{ scale: [null, 1.2, 1.2] }} transition={{ duration: 0.3 }}>
+                                <WrapItem>
+                                    <Center w='150px' h='150px' bg='tomato'>
+                                        <Link href="https://verozuliani.github.io/xero-bank-loans/" isExternal><Image w='150px' h='150px' src="/proyecto3.png" alt=""/></Link>
+                                    </Center>
+                                </WrapItem>
+                            </motion.div>
+                            <motion.div whileHover={{ scale: [null, 1.2, 1.2] }} transition={{ duration: 0.3 }}>
+                                <WrapItem>
+                                    <Center w='150px' h='150px' bg='blue.200'>
+                                        <Link href="https://verozuliani.github.io/photo-gallery/" isExternal><Image w='150px' h='150px' src="/proyecto4.png" alt=""/></Link>
+                                    </Center>
+                                </WrapItem>
+                            </motion.div>
+                            <motion.div whileHover={{ scale: [null, 1.2, 1.2] }} transition={{ duration: 0.3 }}>
+                                <WrapItem>
+                                    <Center w='150px' h='150px' bg='blue.200'>
+                                        <Link href="https://verozuliani.github.io/API-Star-Wars/" isExternal><Image w='150px' h='150px' src="/proyecto5.png" alt=""/></Link>
+                                    </Center>
+                                </WrapItem>
+                            </motion.div>
+                            <motion.div whileHover={{ scale: [null, 1.2, 1.2] }} transition={{ duration: 0.3 }}>
+                                <WrapItem>
+                                    <Center w='150px' h='150px' bg='blue.200'>
+                                        Box 6
+                                    </Center>
+                                </WrapItem>
+                            </motion.div>
                         </Wrap>
 
                     </TabPanel>
                     
                     <TabPanel>
-                        <Wrap justify='space-between'  spacing='40px'
-                            w='700px' h='400px' mt='20px' 
-                            >
-                            <motion.WrapItem 
-                                whileHover={{ scale: [null, 1.1, 1.1] }}
-                                transition={{ duration: 0.3 }}
-                                >
-                                <Center w='150px' h='150px' bg='blue.200'>
-                                    Box 1
-                                </Center>
-                            </motion.WrapItem>
-                            <motion.WrapItem 
-                                whileHover={{ scale: [null, 1.1, 1.1] }}
-                                transition={{ duration: 0.3 }}
-                                >
-                                <Center w='150px' h='150px' bg='blue.200'>
-                                    Box 2
-                                </Center>
-                            </motion.WrapItem>
-                            <motion.WrapItem 
-                                whileHover={{ scale: [null, 1.1, 1.1] }}
-                                transition={{ duration: 0.3 }}
-                                >
-                                <Center w='150px' h='150px' bg='blue.200'>
-                                    Box 3
-                                </Center>
-                            </motion.WrapItem>
+                        <Wrap justify='space-between'  spacing='40px' w='700px' h='400px' mt='20px'>
+                            <motion.div whileHover={{ scale: [null, 1.2, 1.2] }} transition={{ duration: 0.3 }}>
+                                <WrapItem>
+                                    <Center w='150px' h='150px' bg='blue.200'>
+                                        Box 1
+                                    </Center>
+                                </WrapItem>
+                            </motion.div>
+                            <motion.div whileHover={{ scale: [null, 1.2, 1.2] }} transition={{ duration: 0.3 }}>
+                                <WrapItem>
+                                    <Center w='150px' h='150px' bg='blue.200'>
+                                        Box 2
+                                    </Center>
+                                </WrapItem>
+                            </motion.div>
+                            <motion.div whileHover={{ scale: [null, 1.2, 1.2] }} transition={{ duration: 0.3 }}>
+                                <WrapItem>
+                                    <Center w='150px' h='150px' bg='blue.200'>
+                                        Box 3
+                                    </Center>
+                                </WrapItem>
+                            </motion.div>
                         </Wrap>
                     </TabPanel>
 
                     <TabPanel>
-                        <Wrap justify='space-between'  spacing='40px'
-                            w='700px' h='400px' mt='20px'
-                            >
-                            <motion.WrapItem 
-                                whileHover={{ scale: [null, 1.1, 1.1] }}
-                                transition={{ duration: 0.3 }}
-                                >
-                                <Center w='150px' h='150px' bg='blue.200'>
-                                    Box 1
-                                </Center>
-                            </motion.WrapItem>
-                            <motion.WrapItem 
-                                whileHover={{ scale: [null, 1.1, 1.1] }}
-                                transition={{ duration: 0.3 }}
-                                >
-                                <Center w='150px' h='150px' bg='blue.200'>
-                                    Box 2
-                                </Center>
-                            </motion.WrapItem>
+                        <Wrap justify='space-between'  spacing='40px' w='700px' h='400px' mt='20px'>
+                            <motion.div whileHover={{ scale: [null, 1.2, 1.2] }} transition={{ duration: 0.3 }}>
+                                <WrapItem>
+                                    <Center w='150px' h='150px' bg='blue.200'>
+                                        Box 1
+                                    </Center>
+                                </WrapItem>
+                            </motion.div>
+                            <motion.div whileHover={{ scale: [null, 1.2, 1.2] }} transition={{ duration: 0.3 }}>
+                                <WrapItem>
+                                    <Center w='150px' h='150px' bg='blue.200'>
+                                        Box 2
+                                    </Center>
+                                </WrapItem>
+                            </motion.div>
                         </Wrap>
                     </TabPanel>
 
                     <TabPanel>
-                        <Wrap justify='space-between'  spacing='40px'
-                            w='700px' h='400px' mt='20px'
-                            >
-                            <motion.WrapItem 
-                                whileHover={{ scale: [null, 1.1, 1.1] }}
-                                transition={{ duration: 0.3 }}
-                                >
-                                <Center w='150px' h='150px' bg='blue.200'>
-                                    Box 1
-                                </Center>
-                            </motion.WrapItem>
+                        <Wrap justify='space-between'  spacing='40px' w='700px' h='400px' mt='20px'>
+                            <motion.div whileHover={{ scale: [null, 1.2, 1.2] }} transition={{ duration: 0.3 }}>
+                                <WrapItem>
+                                    <Center w='150px' h='150px' bg='blue.200'>
+                                        Box 1
+                                    </Center>
+                                </WrapItem>
+                            </motion.div>
                         </Wrap>
                     </TabPanel>
 
@@ -164,27 +143,26 @@ const Proyectos = () => {
             </Tabs>
 
 
-            
-            <motion.button class="buttonProyectos" 
-                mt='20px'
-                mb='14px'
-                p='5'
-                borderRadius='5px'
-                border='1px'
-                borderStyle='solid'
-                borderColor='text.formLabel'
-                // cursor='pointer'
-                bg='bg.white'
-                transition={{bg:'1s', colorScheme:'1s',}}
-                _hover={{
-                        bg:'text.color',
-                        colorScheme:'text.formLabel',
-                }}
-                whileHover={{ scale: [null, 1.1, 1.1] }}
-                > 
-                Cargar Mas
-            </motion.button> 
-
+            <motion.div whileTap={{scale: 0.85}}>
+                <Button className="buttonProyectos" 
+                    mt='20px'
+                    mb='14px'
+                    p='5px'
+                    borderRadius='5px'
+                    border='1px'
+                    borderStyle='solid'
+                    borderColor='text.formLabel'
+                    bg='bg.white'
+                    fontFamily='font.playfair'
+                    fontWeight='weight.400'
+                    transition={{bg:'1s', color:'1s',}}
+                    _hover={{
+                            bg:'text.color',
+                            color:'text.formLabel',
+                    }}> 
+                    Cargar Mas
+                </Button> 
+            </motion.div>
         </Flex>
 
     )
